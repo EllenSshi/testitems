1c949663ebfecc2f9f39978ed7deee681ffecb80
Elena Shishkina <ellensshi@gmail.com>
2019-10-21 14:39:40 +0300

    add conftest and test_items files

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None, help="Choose language: en, ru, es, ar, ca, cs, da, de, el, fi, fr, it, ko, nl, pl, pt, pt-br, ro, sk, uk, zh-cn")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    lang = request.config.getoption("language")
+    options = Options()
+    if lang is not None:
+        print("\nstart using language for test")
+    else:
+        raise pytest.UsageError("language should be added, for example, en, ru, es, ar, ca, cs, da, de, el, fi, fr, it, ko, nl, pl, pt, pt-br, ro, sk, uk, zh-cn")
+    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    time.sleep(5)
+    print("\nquit browser..")
+    browser.quit()
