6c40e3798401896eaac53ac5f447f86597481fab
EllenSshi <56827662+EllenSshi@users.noreply.github.com>
2019-10-21 15:28:30 +0300

    Update test_items.py
    comment sleep 30 sec

--- test_items.py
+++ test_items.py
@@ -8,5 +8,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_contains_add_to_basket_button(browser):
     browser.get(link)
-    time.sleep(30)
+    #  time.sleep(30)
     assert len(browser.find_elements_by_class_name("btn-add-to-basket")) > 0, "add-to-basket button is absent"
